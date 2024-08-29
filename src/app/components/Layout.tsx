81d0c650009fd4b0c7c356f40571e5b437c3ff31
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-08-29 08:55:23 +0100

    update

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,8 +1,12 @@
 import Footer from './Footer'
 import Navbar from './Navbar'
+import { ReactNode } from 'react'
 
+interface LayoutProps {
+  children: ReactNode;
+}
 
-export default function Layout({ children }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-black text-white">
       <Navbar />
