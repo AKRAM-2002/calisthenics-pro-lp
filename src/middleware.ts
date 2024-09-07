453d9386add084e78728ed81fdfc75d77cc8afc5
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-09-07 15:38:37 +0100

    MongoDb and clerk web hooks Done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+
+// export default clerkMiddleware();
+
+export default authMiddleware({
+  publicRoutes: ['/', '/contact', '/api/webhooks/clerk'],
+})
+
 
 export const config = {
   matcher: [
