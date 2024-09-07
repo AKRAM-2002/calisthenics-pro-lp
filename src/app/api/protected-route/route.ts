e171bcb11d563c4d4d0516bfc7a23e522d8b8956
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-09-07 13:48:09 +0100

    update

--- src/app/api/protected-route/route.ts
+++ src/app/api/protected-route/route.ts
@@ -1,8 +1,9 @@
 import { getAuth } from "@clerk/nextjs/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const { userId } = getAuth();
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const { userId } = getAuth(req);
   
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
